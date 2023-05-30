4cfc1cac721af8c9a0758c3714a9b8dc7e84e5ee
Zohaib Mughal <Zohaibattari106@gmail.com>
2023-05-30 08:04:56 -0700

    first change

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
   params: { hl: "en", gl: "US" },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+    "X-RapidAPI-Key":"41225ef24amsh8e1061f768c3f14p1f8b54jsna343a5b1019c",
     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
   },
 };
@@ -16,5 +16,3 @@ export const fetchDataFromApi = async (url) => {
 };
 // =================================IMP FOR YOU==========================================
 //    You can get easily your API KEY from there https://rapidapi.com/Glavier/api/youtube138/
-
-
