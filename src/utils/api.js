9ec3e294f9e5b9d2622907a3997f7b24254b2d71
Zohaib Mughal <113281959+Mzohaibm@users.noreply.github.com>
2023-06-14 11:36:52 -0700

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
   params: { hl: "en", gl: "US" },
   headers: {
-    "X-RapidAPI-Key":"41225ef24amsh8e1061f768c3f14p1f8b54jsna343a5b1019c",
+    "X-RapidAPI-Key":"Your Api key",
     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
   },
 };
